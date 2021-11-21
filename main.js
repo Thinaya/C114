bd9efee4a8d994cb114f55be4386efc1bb4126e1
Thinaya <81453008+Thinaya@users.noreply.github.com>
2021-11-21 12:27:39 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+noseX=0;
+noseY=0;
+
 function preload() {
+    mustanche = loadImage('https://i.postimg.cc/d3XgHYn4/Mustanche-removebg-preview.png');
 }
 
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(350,300);
+    video.size(300,300);
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -17,8 +21,10 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX= results[0].pose.nose.x;
+        noseY= results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
 
@@ -27,8 +33,9 @@ function modelLoaded() {
 }
 
 function draw() {
-    image(video, 0, 0, 350, 300);
-}
+    image(video, 0, 0, 300, 300);
+    image(mustanche, noseX-30, noseY-10, 60, 50);
+    }
 
 function take_snapshot(){
     save('myFilterImage.png')
